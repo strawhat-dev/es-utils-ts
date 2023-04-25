35636a0fd05737a43e21a82d61f29fa002842011
strawhat-dev <kn37.dev@gmail.com>
2023-04-25 02:24:12 -0700

    regenerate bundle

--- dist/scratch.js
+++
@@ -1,8 +0,0 @@
-import { map } from './chunk-EIGQOOZS.js';
-import './chunk-AYRVJ5K7.js';
-import './chunk-RKAOVNDO.js';
-import './chunk-HXRSFH6L.js';
-
-// src/scratch.ts
-var data = { a: [1, 2, 3], b: [4, 5, 6], c: [7, 8, 9] };
-console.log(map(data, (_, value) => ({ nums: value })));
