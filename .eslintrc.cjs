df2fff5c14015697f6b50c580b09be165e33a3b4
strawhat-dev <kn37.dev@gmail.com>
2023-04-25 00:36:53 -0700

    update configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,21 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
-  ignorePatterns: 'dist',
   env: { es2022: true, node: true },
-  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['.eslintrc.cjs', 'tsup.config.ts', 'dist', '*.d.ts'],
   extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+  plugins: ['@typescript-eslint', 'tree-shaking'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 'latest',
     tsconfigRootDir: __dirname,
     project: ['tsconfig.json'],
+  },
+  rules: {
+    'tree-shaking/no-side-effects-in-initialization': [
+      2,
+      { noSideEffectsWhenCalled: [{ function: 'Object.freeze' }] },
+    ],
   },
 };
--- tsup.config.ts
+++ tsup.config.ts
@@ -2,14 +2,15 @@ import { defineConfig } from 'tsup';
 import { globbySync as glob } from 'globby';
 
 export default defineConfig({
-  entry: glob('src/**/!(types).ts'),
   outDir: 'dist',
   format: 'esm',
   target: 'es2020',
   platform: 'neutral',
   clean: true,
+  keepNames: true,
   splitting: true,
   treeshake: true,
   dts: { resolve: true },
   skipNodeModulesBundle: true,
+  entry: glob('src/**/!(types).ts', { ignore: ['**/*.d.ts'] }),
 });
