233a9709c19a3864f3185ac4a93c09c4a0ebfa32
strawhat-dev <kn37.dev@gmail.com>
2023-05-07 05:27:03 -0700

    optimization

--- src/range.ts
+++ src/range.ts
@@ -22,28 +22,6 @@ export const range = (start = 0, stop?: number, step?: number) => {
 };
 
 /**
- * Like {@link range}, except that an object *(where both the
- * keys and values are numbers within the provided range)* is returned
- * instead. This would allow one to semantically check if a given number
- * falls within a range similarly to *Python*.
- *
- * @example
- * if (score in Range(90, 100)) return 'A';
- */
-export const Range = (start = 0, stop?: number, step?: number) => {
-  const result = {};
-  if (not(stop)) [start, stop] = [0, start];
-  if (not(step)) step = stop < start ? -1 : 1;
-  let n = Math.max(0, Math.ceil((stop - start) / (step || 1)));
-  while (n--) {
-    result[start] = start;
-    start += step;
-  }
-
-  return result as Record<number, number>;
-};
-
-/**
  * Note: A decreasing range (`step = -1`) will be
  * automatically inferred if not specified and `start > stop`
  * or only a single negative `start` value is given.
@@ -57,16 +35,55 @@ export const irange = ((...args) => {
 }) as typeof range;
 
 /**
- * Like {@link irange}, except that an object *(where both the
- * keys and values are numbers within the provided range)* is returned
- * instead. This would allow one to semantically check if a given number
- * falls within a range similarly to *Python*.
+ * Allows one to semantically check if a given number
+ * falls within a range similarly to *Python* by utilizing
+ * a `Proxy` and creating a trap for the `in` operator.
+ *
+ * @see {@link range} for normal range usage
+ *
+ * @example
+ * if (score in Range(90, 100)) return 'A';
+ */
+export const Range = (start = 0, stop?: number, step?: number) => {
+  if (not(stop)) [start, stop] = [0, start];
+  if (not(step)) step = stop < start ? -1 : 1;
+  return new Proxy({} as object, {
+    has(_, n: string) {
+      if (!/^\d+$/.test(n)) return false;
+      if (+n % step!) return false;
+      if (!step) return +n === start;
+      if (start > stop!) {
+        return +n <= start && +n > stop!;
+      }
+
+      return +n >= start && +n < stop!;
+    },
+  });
+};
+
+/**
+ * Allows one to semantically check if a given number
+ * falls within an ***inclusive*** range similarly to *Python* by
+ * utilizing a `Proxy` and creating a trap for the `in` operator.
+ *
+ * @see {@link irange} for normal irange usage
  *
  * @example
  * if (score in iRange(90, 100)) return 'A';
  */
-export const iRange = ((...args) => {
-  const [i, start] = args.length > 1 ? [1, args[0]] : [0, 0];
-  args[i]! < start! ? --args[i]! : ++args[i]!;
-  return Range(...args);
-}) as typeof Range;
+export const iRange = (start = 0, stop?: number, step?: number) => {
+  if (not(stop)) [start, stop] = [0, start];
+  if (not(step)) step = stop < start ? -1 : 1;
+  return new Proxy({} as object, {
+    has(_, n: string) {
+      if (!/^\d+$/.test(n)) return false;
+      if (+n % step!) return false;
+      if (!step) return +n === start;
+      if (start > stop!) {
+        return +n <= start && +n >= stop!;
+      }
+
+      return +n >= start && +n <= stop!;
+    },
+  });
+};
