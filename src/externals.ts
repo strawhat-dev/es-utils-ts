11b73254fb0a6a85f500e725ae43a1105fae81d8
strawhat-dev <kn37.dev@gmail.com>
2023-05-06 20:55:58 -0700

    update deps

--- src/externals.ts
+++ src/externals.ts
@@ -1,7 +1,9 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable tree-shaking/no-side-effects-in-initialization */
 // @ts-ignore
-import rfdc from 'rfdc/default';
-import equal from 'deep-equal';
+import clone from 'rfdc/default';
+import rfdc from 'rfdc';
+import deepequal from 'deep-equal';
 
 export {
   deepmerge,
@@ -16,10 +18,33 @@ export { default as inspect } from 'object-inspect';
 
 export { default as escapeRegex } from 'escape-string-regexp';
 
-export const deepclone = rfdc as <T>(source: T) => T;
+/**
+ * *rfdc (Really Fast Deep Clone)* w/ circular reference support
+ *
+ * Note: 25% additional performance overhead with circular reference support.
+ * Use {@link deepclone} if the object is guaranteed to have no circular references.
+ * @see {@link https://www.npmjs.com/package/rfdc}
+ */
+export const deepcopy = rfdc?.({ circles: true });
 
+/**
+ * *rfdc (Really Fast Deep Clone)*
+ *
+ * Note: Will **fail** if the object contains circular references.
+ * Use {@link deepcopy} if the object can contain circular references.
+ * @see {@link https://www.npmjs.com/package/rfdc}
+ */
+export const deepclone = clone as <T>(source: T) => T;
+
+/**
+ * *deep-equal* module w/ strict **on** by default
+ *
+ * Compare objects `a` and `b`, returning whether they
+ * are equal according to a recursive equality algorithm.
+ * @see {@link https://www.npmjs.com/package/deep-equal}
+ */
 export const deepcompare = (
   a: unknown,
   b: unknown,
-  { strict = true }: { strict?: boolean } = {}
-) => equal(a, b, { strict });
+  options: { strict: boolean } = { strict: true }
+) => deepequal(a, b, options);
