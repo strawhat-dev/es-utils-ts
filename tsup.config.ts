151d7e250042fc76e9d728ad4f23c87b9b964515
strawhat-dev <kn37.dev@gmail.com>
2023-05-09 21:46:55 -0700

    update config

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,15 +2,15 @@ import { defineConfig } from 'tsup';
 import { globbySync as glob } from 'globby';
 
 export default defineConfig({
-  entry: ['src/path.ts', 'src/externals.ts', ...glob('src/**/index.ts')],
-  outDir: 'dist',
-  format: 'esm',
-  target: 'es2020',
   platform: 'neutral',
+  target: 'es2020',
+  format: 'esm',
   clean: true,
   keepNames: true,
   splitting: true,
   treeshake: true,
   dts: { resolve: true },
   skipNodeModulesBundle: true,
+  outDir: 'dist',
+  entry: ['src/path.ts', 'src/externals.ts', ...glob('src/**/index.ts')],
 });
