3ac4651d7050f4613bbc2b1576beb93929921b86
strawhat-dev <kn37.dev@gmail.com>
2023-05-11 13:44:51 -0700

    rm functional operators

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,14 +4,3 @@ export * from './range';
 // prettier-ignore
 /** trims a given string while also removing all empty lines */
 export const trimLines = (s: string) => s.split(/\r?\n/).filter((line) => line.trim()).join('\n');
-
-// functional operators
-export const add = (a: number, b: number) => a + b;
-export const sub = (a: number, b: number) => a - b;
-export const mult = (a: number, b: number) => a * b;
-export const div = (a: number, b: number) => a / b;
-export const mod = (a: number, b: number) => a % b;
-export const lt = (a: number, b: number) => a < b;
-export const gt = (a: number, b: number) => a > b;
-export const lte = (a: number, b: number) => a <= b;
-export const gte = (a: number, b: number) => a >= b;
